cf7db6ca47164a656a46f3b6ecdc4043d28d4ced
yatsun <yatsunlam25@gmail.com>
2021-03-30 01:09:55 -0400

    commit tree b302a49ea6eab729c2eb39c4c3c8501cc2b61ba9

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("http://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+doc.css(".tout__label.heading.heading--level-4") 
+
